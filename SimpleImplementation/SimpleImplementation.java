73db0c6b19d8f9eeeda48779f456acb58de5c9a2
PeterRanario <peterranario11@gmail.com>
2024-09-25 14:35:14 +0800

    Simple Implentation of Array

---
+++ SimpleImplementation/SimpleImplementation.java
@@ -0,0 +1,10 @@
+package SimpleImplementation;
+
+public class SimpleImplementation {
+
+    public static void main(String[] args) {
+        int[] numbers = {5, 10, 15, 20};
+
+        System.out.println("Element in array: " + numbers[0]);
+    }
+}

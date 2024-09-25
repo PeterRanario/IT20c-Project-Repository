0323adc006850781843282d7a220d30d9037bce9
PeterRanario <peterranario11@gmail.com>
2024-09-25 14:46:10 +0800

    Display Via For Loop

--- SimpleImplementation/SimpleImplementation.java
+++ SimpleImplementation/SimpleImplementation.java
@@ -5,6 +5,6 @@ public class SimpleImplementation {
     public static void main(String[] args) {
         int[] numbers = {5, 10, 15, 20};
 
-        System.out.println("Element in array: " + numbers[0]);
+        System.out.println("First element in array: " + numbers[0]);
     }
 }

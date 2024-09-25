466da0de115252b62570ba9f00959d2e41f5f482
PeterRanario <peterranario11@gmail.com>
2024-09-25 14:24:03 +0800

    Simple Implementation of Array

---
+++ it20c/arrays/It20cArrays.java
@@ -0,0 +1,12 @@
+package it20c.arrays;
+
+public class It20cArrays {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] numbers = {5, 10, 15, 20};
+
+        System.out.println("First element in array: " + numbers[0]);
+    }
+
+}

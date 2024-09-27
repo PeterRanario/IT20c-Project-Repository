2e42330cdd4ff14360ce93e070e861602e5da9b7
PeterRanario <peterranario@gmail.com>
2024-09-27 11:29:42 +0800

    ArrayList

---
+++ main/java/Array_List/ItemManager.java
@@ -0,0 +1,38 @@
+package Array_List;
+
+import java.util.ArrayList;
+
+public class ItemManager {
+    
+     private ArrayList<String> items;
+
+    public ItemManager() {
+        items = new ArrayList<>();
+    }
+
+    public void addItem(String item) {
+        items.add(item);
+        System.out.println(item + " is added.");
+    }
+
+    public void removeItem(String item) {
+        if (items.remove(item)) {
+            System.out.println(item + " is removed.");
+        } else {
+            System.out.println(item + " not found.");
+        }
+    }
+    
+    public void displayItems() {
+        if (items.isEmpty()) {
+            System.out.println("List is empty.");
+        } else {
+            System.out.println("Items in the list:");
+            for (String item : items) {
+                System.out.println(" " + item);
+            }
+        }
+    }
+}
+
+
